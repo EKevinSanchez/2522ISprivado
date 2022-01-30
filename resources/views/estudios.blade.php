266f19461af92604981225a0e1eb310cf59757c9
Xvegetta <72033570+Xvegetta@users.noreply.github.com>
2022-01-30 16:10:00 -0600

    update

---
+++ resources/views/estudios.blade.php
@@ -0,0 +1,18 @@
+@extends('template.layout')
+
+@section('tituloSeccion')
+  <h3 class="section-title">estudios profesionales</h3>
+@endsection
+@section('contenido')
+    En esta seccion se muestran los estudios profesionales que he realizado. Tomando como inicio los estudios de media superior en CBT No. 2 Lic Carlos Pichardo, Tecamac
+@endsection
+@section('tituloSegundo')
+    Media Superior
+@endsection
+@section('textoSegundo')
+    Estudios de media superior en CBT No. 2 Lic Carlos Pichardo, Tecamac en la carrera de Tecnico en Informatica. 
+    <br>
+    Durante este periodo he realizado cursos de programacion, diseño y desarrollo de software, asi como desarrollo de pequeños proyectos de pagina web.
+    <br>
+    Lo mas tracedental fue mi participacion en un equipo internacional de robotica, donde junto a mis compañeros fuimos los pioneros de este proyecto en la escuela.
+@endsection
